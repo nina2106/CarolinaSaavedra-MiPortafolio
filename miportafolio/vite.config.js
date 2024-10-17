064ca6403477a27ea49c73c965055c761ad23642
carolina saavedra <carito2106.saavedra@gmail.com>
2024-10-16 23:44:15 -0500

    caro

--- miportafolio/vite.config.js
+++ miportafolio/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// vite.config.js
+export default {
+  // other configurations...
+  server: {
+    hmr: {
+      overlay: false, // Disable the HMR overlay
+    },
+  },
+};
