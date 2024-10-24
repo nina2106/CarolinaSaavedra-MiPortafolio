5e9045927996f635394124e320857960c21d5b60
carolina saavedra <carito2106.saavedra@gmail.com>
2024-10-23 23:46:32 -0500

    Carolina

--- miportafolio/src/components/About.jsx
+++ miportafolio/src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         className="absolute inset-0 w-full h-full object-cover" // GIF cubriendo toda la pantalla
       />
       {/* Contenedor del texto en caja */}
-      <div className="relative z-10 p-6 rounded-lg shadow-lg text-left text-blue-700"> {/* Cambiado a text-left y max-w-2xl para limitar el ancho */}
+      <div className="relative z-10 p-6 rounded-lg shadow-lg text-left text-yellow-100"> {/* Cambiado a text-left y max-w-2xl para limitar el ancho */}
         <h1 className="text-9xl text-blue-200 font-bold mb-6">Sobre Mí</h1> {/* Aumenta el tamaño del título */}
         <p className="text-4xl mb-8">
           Soy diseñador web con 5 años de experiencia en rem Agency. Acabo de terminar mis estudios en el SENA y estoy listo para nuevos desafíos. Durante mis estudios, he trabajado en varios proyectos académicos relacionados con el desarrollo de aplicaciones web, lo que me ha permitido fortalecer mis habilidades en HTML, CSS, JavaScript, y React. Me apasiona el diseño de experiencias de usuario, el trabajo en equipo, y siempre busco mejorar mis habilidades. Además, tengo un fuerte interés en continuar aprendiendo sobre desarrollo frontend y diseño para seguir creciendo profesionalmente.
