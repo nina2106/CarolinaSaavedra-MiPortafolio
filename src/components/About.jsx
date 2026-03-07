1a268dbad13da16ebfec767f7cf2ebcec7613376
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-06 23:01:16 -0500

    carolina

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
       <div className="relative z-10 flex items-center justify-start w-full max-w-8xl">
         
         {/* Contenedor de la imagen, más grande */}
-        <div className="flex-shrink-0 mr-9">
+        <div className="flex-shrink-0 mr-10">
           <img
             src={img4}
             alt="Mi imagen"
