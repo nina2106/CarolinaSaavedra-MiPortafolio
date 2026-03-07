095331059eb567dcc8de67404f71fa36e6dbe57a
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-06 22:44:17 -0500

    carolina

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import img4 from '../assets/imagen/IMG1.jpg';
 
 const About = () => {
   return (
-    <div className="relative flex items-center justify-center min-h-screen p-8">
+    <div className="relative flex items-center justify-center min-h-screen p-4">
 
       {/* Imagen de fondo */}
       <img
