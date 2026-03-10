70f68371dc5c9dbb57645f09eaa67631fbb4d61e
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-10 13:59:30 -0500

    carolina

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,34 +12,30 @@ const About = () => {
         className="absolute inset-0 w-full h-full object-cover"
       />
 
-      {/* Contenedor principal más grande con flex para dividir imagen y texto */}
-      <div className="relative z-10 flex items-center justify-start w-full max-w-8xl">
+      <div className="relative z-10 flex items-center w-full max-w-6xl">
 
-        {/* Contenedor de la imagen, más grande */}
-        <div className="relative z-10 flex items-center justify-start w-full max-w-6xl">
-
-          {/* Contenedor de la imagen */}
-          <div className="flex-shrink-0 mr-12">
-            <img
-              src={img4}
-              alt="Mi imagen"
-              className="w-73 h-73 object-cover rounded-full shadow-lg"
-            />
-          </div>
-
+        {/* Imagen */}
+        <div className="flex-shrink-0 mr-12">
+          <img
+            src={img4}
+            alt="Mi imagen"
+            className="w-56 h-56 object-cover rounded-full shadow-lg"
+          />
         </div>
 
-        {/* Contenedor del texto más amplio */}
-        <div className="text-white max-w-4xl">
-          <h1 className="text-7xl text-green-400 text-center font-bold mb-6">Sobre Carolina Saavedra</h1>
-          <p className="text-4xl text-center">
-            Desarrolladora Web Full Stack Junior y Diseñadora de Interfaces Web con 5 años de experiencia en la creación de soluciones digitales. Experiencia en diseño de contenido visual para proyectos web,
-            incluyendo gráficos para redes sociales, banners y materiales digitales. Con conocimientos en tecnologías como JavaScript, React, React Native, Flutter, PHP, Node.js, Python, MySQL, Firebase, HTML y
-            Tailwind CSS,Python. Me destaco por ser responsable, autodidacta, trabajar en equipo y cumplir objetivos en el desarrollo de proyectos tecnológicos.
-          </p>
-        </div>
+      </div>
+
+      {/* Contenedor del texto más amplio */}
+      <div className="text-white max-w-4xl">
+        <h1 className="text-7xl text-green-400 text-center font-bold mb-6">Sobre Carolina Saavedra</h1>
+        <p className="text-4xl text-center">
+          Desarrolladora Web Full Stack Junior y Diseñadora de Interfaces Web con 5 años de experiencia en la creación de soluciones digitales. Experiencia en diseño de contenido visual para proyectos web,
+          incluyendo gráficos para redes sociales, banners y materiales digitales. Con conocimientos en tecnologías como JavaScript, React, React Native, Flutter, PHP, Node.js, Python, MySQL, Firebase, HTML y
+          Tailwind CSS,Python. Me destaco por ser responsable, autodidacta, trabajar en equipo y cumplir objetivos en el desarrollo de proyectos tecnológicos.
+        </p>
       </div>
     </div>
+    </div >
   );
 };
 
