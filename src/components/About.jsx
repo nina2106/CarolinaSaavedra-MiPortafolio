028eb14abc749c5c0dbab82c505598debea55289
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-10 17:47:05 -0500

    carolina

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const About = () => {
             <img
               src={img4}
               alt="Mi imagen"
-              className="w-90 h-8 object-cover rounded-full shadow-lg"
+              className="w-56 h-56 object-cover rounded-full shadow-lg"
             />
           </div>
 
