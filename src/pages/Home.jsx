5abf098b6575db0c889e98626fba2cac860048da
carolina saavedra <carito2106.saavedra@gmail..com>
2026-03-10 21:52:44 -0500

    carolina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,9 +13,9 @@ const Home = () => {
       />
       {/* Contenedor principal con texto centrado */}
       <div className="relative z-10 bg-blue bg-opacity-20 rounded-lg shadow-md text-center max-w-4xl p-8">
-        <h1 className="text-6xl text-blue-200 font-bold mb-6">
+        <h1 className="text-6xl text-blue-300 font-bold mb-6">
           Hola, soy María Carolina Saavedra Garzón</h1>
-        <p className="text-7xl text-blue-200 mb-8">
+        <p className="text-7xl text-blue-300 mb-8">
           Desarrolladora FullStack <h1>y Diseñadora UI</h1>
         </p>
         <a
